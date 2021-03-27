4cc65a41a17cd3ba79dc31ed67098e83dc2d49b4
Patrick PIGNOL <patrick.pignol@gmail.com>
2021-03-27 21:58:40 +0100

    Update memccpy.c

--- memccpy.c
+++ memccpy.c
@@ -6,3 +6,32 @@
  */
 
 
+#include "libft.h"
+
+void* memccpy(void* pDestination, void* pSource, int pChar, size_t pNumber)
+{
+	size_t vIndex;
+
+	vIndex = 0;
+
+	if(pDestination != NULL)
+	{
+		if(pSource != NULL)
+		{
+			unsigned char* vSource;
+			unsigned char vChar;
+			vSource = (unsigned char*) pSource;
+			vChar = pChar;
+			while(vIndex < pNumber)
+			{
+				memmove(&pDestination[vIndex], &pSource[vIndex], 1);
+				if(vSource[vIndex] == pChar)
+				{
+					return &pDestination[vIndex + 1];
+				}
+				vIndex++;
+			}
+		}
+	}
+	return
+}
